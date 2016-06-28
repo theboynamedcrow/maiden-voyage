e329c4792895ea861ce1decaf084615c5d4376a8
tbrown <tbrown@hamilton.edu>
2016-06-28 11:20:10 -0400

    changed model to remove User, use auth functionality instead

--- leagueTracker/models.py
+++ leagueTracker/models.py
@@ -1,17 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    display_name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=70, null=True, unique=True)
-    date_joined = models.DateTimeField('date joined')
-    def __str__(self):
-        return self.display_name
-
-
 class Golfer(models.Model):
-    user = models.ForeignKey(User)
-    name = models.CharField(max_length=60)
-    handicap = models.IntegerField(default=0)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=60,blank=True,null=True)
+    name = models.CharField(max_length=60,blank=True,null=True)
+    handicap = models.IntegerField(blank=True, null=True)
     def __str__(self):
         return self.name
